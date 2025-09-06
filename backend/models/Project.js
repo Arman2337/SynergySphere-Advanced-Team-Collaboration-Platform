c5033640dd73db34b251bc087f6ea147353f637d
Arman2337 <23it119@charusat.edu.in>
2025-09-06 16:08:55 +0530

    updated models

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -5,6 +5,15 @@ const ProjectSchema = new mongoose.Schema({
     description: { type: String },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    tags: [{ type: String }],
+    projectManager: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    deadline: { type: Date },
+    priority: {
+        type: String,
+        enum: ['Low', 'Medium', 'High'],
+        default: 'Low'
+    },
+    imageUrl: { type: String, default: '' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Project', ProjectSchema);
