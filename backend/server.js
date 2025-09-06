9ec5f80822cc7458999fda6513b1b3cf7082a95f
Arman2337 <23it119@charusat.edu.in>
2025-09-06 16:09:09 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,9 @@ app.use(cors({
 app.use(express.json()); // Body parser for JSON
 app.use(cookieParser());
 
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 // Define API Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
